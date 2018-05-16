3b657b7c7aa7f9dddeca2d44fe0e3ad54d34fa24
Robert Lucian CHIRIAC <robert.lucian.chiriac@gmail.com>
2018-05-16 13:52:42 +0300

    hotfix - use http instead of https for apt-get source

--- scripts/install_tools.sh
+++ scripts/install_tools.sh
@@ -123,8 +123,8 @@ update_install_aptget() {
       # add gpg key for nodejs
       curl -s https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo apt-key add -
       # add nodejs to apt-get source list
-      sudo sh -c "echo 'deb https://deb.nodesource.com/node_9.x $OS_CODENAME main' > /etc/apt/sources.list.d/nodesource.list"
-      sudo sh -c "echo 'deb-src https://deb.nodesource.com/node_9.x $OS_CODENAME main' >> /etc/apt/sources.list.d/nodesource.list"
+      sudo sh -c "echo 'deb http://deb.nodesource.com/node_9.x $OS_CODENAME main' > /etc/apt/sources.list.d/nodesource.list"
+      sudo sh -c "echo 'deb-src http://deb.nodesource.com/node_9.x $OS_CODENAME main' >> /etc/apt/sources.list.d/nodesource.list"
       echo "Added NodeJS repo to source for apt-get for \"$OS_CODENAME\" distribution"
     else
       echo "Couldn't add Nodejs repo to source because it's not available for \"$OS_CODENAME\" distribution"
